ba636dc47380e4bccc044dafe2184accdcb1d455
bukolajanet <90992585+bukolajanet@users.noreply.github.com>
2022-01-26 11:34:02 +0000

    Calculating yearly spend .

---
+++ Spending_calculator .py
@@ -0,0 +1,10 @@
+''' 
+Defining a variable named spend_per_week which is 300 and then calculating spens_per_year.
+'''
+name = "Dan"
+spend_per_week = 300
+spend_per_year = spend_per_week * 52 
+print (name +"'s yearly spend is: ")
+print(spend_per_year)
+
+''' Note ; Nunber of weeks in a year is 52'''
